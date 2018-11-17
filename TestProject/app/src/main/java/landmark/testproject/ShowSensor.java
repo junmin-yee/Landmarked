4525ca76a940014d024eb8191700bfa583f65b9d
Junmin Yee <junminyee98@gmail.com>
2018-11-17 12:03:17 -0800

    Created Project to Find Sensors

---
+++ TestProject/app/src/main/java/landmark/testproject/ShowSensor.java
@@ -0,0 +1,32 @@
+package landmark.testproject;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ShowSensor extends AppCompatActivity {
+    TextView tv1=null;
+    private SensorManager mSensorManager;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_sensor);
+
+        tv1 = (TextView) findViewById(R.id.textView2);
+        tv1.setVisibility(View.GONE);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> mList= mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        for (int i = 1; i < mList.size(); i++) {
+            tv1.setVisibility(View.VISIBLE);
+            tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+        }
+    }
+}
