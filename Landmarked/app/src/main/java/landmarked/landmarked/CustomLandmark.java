12bc7f2b488d88c47ec31dd17288e21de4a3bf0f
hammockg98 <43797650+hammockg98@users.noreply.github.com>
2019-02-13 02:40:31 -0800

    Added UI for Landmark page
    The UI for displaying landmarks is shown. If any values need fine-tuning, you know where to reach me.

--- Landmarked/app/src/main/java/landmarked/landmarked/CustomLandmark.java
+++ Landmarked/app/src/main/java/landmarked/landmarked/CustomLandmark.java
@@ -7,8 +7,11 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -29,40 +32,51 @@ public class CustomLandmark extends AppCompatActivity {
         //get user ID that correlates to the database
         landmarkInfo = findViewById(R.id.textView2);
 
-        landmarkInfo.setText("Hello");
+        //landmarkInfo.setText("Hello");
 
         LandmarkList = findViewById(R.id.landmarkList);
 
+        AddElement(32.543, 125.4465, 304);
+        AddElement(43.567, 10.43434, 10000323);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+        AddElement(213.65, 233453, -2);
+    }
+
+    void AddElement(double latitude, double longitude, double elevation)
+    {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(landmarkInfo.getLayoutParams());
-        //LandmarkList.setOrientation(LinearLayout.VERTICAL);
 
         LinearLayout vert = new LinearLayout(this);
         vert.setOrientation(LinearLayout.HORIZONTAL);
-        vert.setBackgroundColor(Color.GREEN);
-        vert.setPadding(20,20,0,20);
+
+        vert.setBackgroundColor(Color.LTGRAY);
+        vert.setPadding(20,20,20,20);
 
         ImageView img = new ImageView(this);
-        img.setBackgroundColor(Color.BLACK);
-        img.setPadding(25,25,25,25);
-        img.setMinimumWidth(300);
-        img.setMinimumHeight(300);
-        img.setMaxWidth(300);
-        img.setMaxHeight(300);
-        img.setImageResource(R.mipmap.logo3);
+        img.setBackgroundColor(Color.WHITE);
+
+        ViewGroup.LayoutParams imgSize = new ViewGroup.LayoutParams(200, 200);
+        img.setImageResource(R.drawable.logo3);
+        img.setLayoutParams(imgSize);
         vert.addView(img);
 
         tempView = new TextView(this);
-        tempView.setText("Hello");
+        tempView.setText("Lat: " + latitude +"\nLong: " + longitude + "\nElv: " + elevation);
+        tempView.setTextColor(Color.BLACK);
+        tempView.setPadding(10,0,0,0);
         tempView.setLayoutParams(params);
         tempView.setTextAppearance(R.style.ListElemText);
         vert.addView(tempView);
 
         LandmarkList.addView(vert);
-
-        tempView = new TextView(this);
-        tempView.setText("Grizzledor");
-        tempView.setLayoutParams(params);
-        tempView.setTextAppearance(R.style.ListElemText);
-        LandmarkList.addView(tempView);
     }
+
 }
