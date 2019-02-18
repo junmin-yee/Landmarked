04854c096d4eb846fb977e764dd1eccc05dea8e9
Unknown <lwfrancisco@gmail.com>
2019-02-17 22:54:39 -0800

    Implements Reverse Geocode Request
    Added function able to make and receive reverse geocode requests.

--- Landmarked/app/src/main/java/landmarked/landmarked/LandmarkRetrieval.java
+++ Landmarked/app/src/main/java/landmarked/landmarked/LandmarkRetrieval.java
@@ -1,20 +1,27 @@
 package landmarked.landmarked;
 
 import android.location.Location;
+import android.util.Log;
 
-import com.mapbox.core.exceptions.ServicesException;
-import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.geojson.Point;
-//import com.mapbox.api.geocoding.v5.models.CarmenFeature
-import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class LandmarkRetrieval {
 
     private SensorData mSensorData;
     private Location mCurrLocation;
-    private FeatureCollection mFeatureCollection;
-    private LatLng mLatLng;
+    List<CarmenFeature> mResults;
 
     public LandmarkRetrieval() {
 
@@ -25,29 +32,65 @@ public class LandmarkRetrieval {
 
     }
 
-    private void makeGeocodeSearch(final LatLng latLng) {
+    private void buildGeocodeSearch(final Location location) {
+        mCurrLocation = location;
 
-        // In progress
+        // Sets Access Token
+        // Constructs query based on current location.
+        // Maximum limit of results for reverse geocoding is 5.
+        // Filters the search to "Points of Interest - Landmark" in TYPE_POI_LANDMARK
+        //      This MAY not be exactly what we want - I'm unsure of everything this entails.
+        //      Delete the "_LANDMARK" to get all POI.
+        // Build simply constructs the query, must be at end.
+        MapboxGeocoding reverseGeocode = MapboxGeocoding.builder()
+                .accessToken("pk.eyJ1IjoicmVkZ3JlZWQ0IiwiYSI6ImNqb2k3NXNpNjAyMGEzcXBhbThoeXBtOGcifQ.AG9JmnzPQKHuSxazOvrk3g")
+                .query(Point.fromLngLat(mCurrLocation.getLongitude(), mCurrLocation.getLatitude()))
+                //.proximity(Point)      // Useful for setting a bias of results toward a specific point - Calculate point in front of user?
+                .limit(5)
+                .geocodingTypes(GeocodingCriteria.TYPE_POI_LANDMARK)
+                .build();
+
+        reverseGeocode.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+
+                mResults = response.body().features();
+
+                if (mResults.size() > 0) {
+
+                    // Log the location of response.
+                    Log.d(TAG, "onResponse: " + mResults.size() + " results at " + location.toString());
+
+                } else {
+
+                    // No results were found.
+                    Log.d(TAG, "onResponse: No result found");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                // Failed to send request
+                throwable.printStackTrace();
+            }
+        });
     }
 
     // Collect nearby Features from Reverse Geocode Search
     public void FilterFeatures(Location location) {
-        mCurrLocation = location;
 
-        // Construct basic custom landmark "point" feature for testing
-        /*Feature feature = Feature.fromGeometry(Point.fromLngLat(mCurrLocation.getLongitude(), mCurrLocation.getLatitude()));
-        //System.out.println(feature.toJson());*/
+        buildGeocodeSearch(location);
 
-        // Make a Latlng object using a location object.
-        mLatLng = new LatLng(mCurrLocation);
-        makeGeocodeSearch(mLatLng);
+        // Use mResults and a set of defined-properties for the user to display. Filter GeoJSON if necessary.
+        // mResults
 
     }
 
-    public FeatureCollection getFeatureCollection() {
+    public List<CarmenFeature> getCarmenFeatureResult() {
 
         // Return list of Carmen Features.
-        return mFeatureCollection;
+        return mResults;
     }
 
 }
