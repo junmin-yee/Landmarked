aabc38a5784a349988424869d1c84781b79e1369
Junmin Yee <junminyee98@gmail.com>
2019-04-13 01:32:02 -0700

    Update LoadingPage.java

--- Landmarked/app/src/main/java/landmarked/landmarked/LoadingPage.java
+++ Landmarked/app/src/main/java/landmarked/landmarked/LoadingPage.java
@@ -34,12 +34,6 @@ public class LoadingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_page);
-    }
-
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
 
         checkLocationPermission();
 
@@ -52,6 +46,12 @@ public class LoadingPage extends AppCompatActivity {
         //Register listeners
         mSensorData.registerOrientationSensors();
         mSensorData.registerLocationSensor();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
 
         currOrientation = mSensorData.getCurrentOrientation();
 
