d26d5372ccb44d60b458b630db906eaae58ab543
kellyjvan <kellyjvan@gmail.com>
2019-05-06 23:01:36 -0700

    added landmarkHistory XAML

--- Landmarked/app/src/main/java/landmarked/landmarked/LandmarkHistory.java
+++ Landmarked/app/src/main/java/landmarked/landmarked/LandmarkHistory.java
@@ -1,8 +1,15 @@
 package landmarked.landmarked;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class LandmarkHistory extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
+
+
+    }
 }
