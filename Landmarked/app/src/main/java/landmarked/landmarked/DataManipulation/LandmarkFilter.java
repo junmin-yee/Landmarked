e121647c2ca748ea97c6ed6da2a4bac1d0eb405c
Unknown <lwfrancisco@gmail.com>
2019-05-21 12:17:24 -0700

    Added placeType filter to LandmarkFilter

--- Landmarked/app/src/main/java/landmarked/landmarked/DataManipulation/LandmarkFilter.java
+++ Landmarked/app/src/main/java/landmarked/landmarked/DataManipulation/LandmarkFilter.java
@@ -7,6 +7,27 @@ import java.util.Set;
 
 public class LandmarkFilter {
 
+    /**placeTypeFilter
+     * Extremely basic filter which removes anything that most certainly isn't a Landmark.
+     *
+     * See https://docs.mapbox.com/api/search/#data-types for different PlaceTypes.
+     *
+     * Crater Lake, for example, is a place.
+     * Parks, Historical sites, etc. are Points of Interest (poi).
+     * Upper Klamath Lake, for example, is an address. As are all streets and such.
+     *
+     * In short: What a mess.
+      */
+    public boolean placeTypeFilter(CarmenFeature feature){
+        // If the CarmenFeature is one of the following, it will not be filtered.
+        return (feature.placeType().contains("poi") || feature.placeType().contains("address") || feature.placeType().contains("place"));
+    }
+
+    /**whitelistFilter
+     *
+     * @param filteredLandmarks
+     * @return
+     */
     public Set<CarmenFeature> whitelistFilter(Set<CarmenFeature> filteredLandmarks) {
 
         Set<CarmenFeature> filter = new HashSet<>();
@@ -19,6 +40,4 @@ public class LandmarkFilter {
 
         return filteredLandmarks;
     }
-
-
 }
