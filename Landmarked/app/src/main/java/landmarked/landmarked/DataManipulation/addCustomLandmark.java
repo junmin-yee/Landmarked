8ff4e626cce8c33b55d4d52dc24baedce95590a7
hammockg98 <43797650+hammockg98@users.noreply.github.com>
2019-05-23 07:17:49 -0700

    Properly checked possible errors input by user input
    Check user input as well as showed some...toast... to the user if there was one that occured.

--- Landmarked/app/src/main/java/landmarked/landmarked/DataManipulation/addCustomLandmark.java
+++ Landmarked/app/src/main/java/landmarked/landmarked/DataManipulation/addCustomLandmark.java
@@ -1,5 +1,7 @@
 package landmarked.landmarked.DataManipulation;
 
+import android.content.Context;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -23,6 +26,7 @@ public class addCustomLandmark extends AppCompatActivity
 {
     private static final int inputType = 12290; //signed and decimal code
     private static final int inputEmpty = 131073;
+    SensorData mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,8 @@ public class addCustomLandmark extends AppCompatActivity
         int inputEnd = temp.getInputType();
 
         int rest = inputEnd;
+
+        mData = LandmarkedMain.getInstance().mSensorData;
     }
 
     public void onCheckClicked(View v)
@@ -45,29 +51,58 @@ public class addCustomLandmark extends AppCompatActivity
             case R.id.currLocationBox:
                 if(currResult)
                 {
+                    Location l = mData.getCurrentLocation();
                     temp = findViewById(R.id.latitudeEntry);
+                    if(l != null)
+                    {
+                        temp.setText(Double.toString(l.getLatitude()));
+                    }
+                    else
+                    {
+                        temp.setText("0");
+                    }
                     temp.setFocusable(false);
                     temp = findViewById(R.id.longitudeEntry);
+                    if(l != null)
+                    {
+                        temp.setText(Double.toString(l.getLongitude()));
+                    }
+                    else
+                    {
+                        temp.setText("0");
+                    }
                     temp.setFocusable(false);
+
                 }
                 else
                 {
                     temp = findViewById(R.id.latitudeEntry);
+                    temp.setText("");
                     temp.setFocusableInTouchMode(true);
                     temp = findViewById(R.id.longitudeEntry);
+                    temp.setText("");
                     temp.setFocusableInTouchMode(true);
                 }
                 break;
             case R.id.elevationBox:
                 if(currResult)
                 {
+                    Location l = mData.getCurrentLocation();
                     temp = findViewById(R.id.elevationEntry);
-                    temp.setText("");
+                    if(l != null)
+                    {
+                        temp.setText(Double.toString(l.getAltitude()));
+                    }
+                    else
+                    {
+                        temp.setText("0");
+                    }
                     temp.setFocusable(false);
                 }
                 else
                 {
                     temp = findViewById(R.id.elevationEntry);
+                    temp.setText("");
                     temp.setFocusableInTouchMode(true);
                 }
                 break;
@@ -76,54 +111,106 @@ public class addCustomLandmark extends AppCompatActivity
 
     public void onSaveCustom(View v)
     {
-        int errorVar = 0;
-        float rangeVal;
-        EditText temp = findViewById(R.id.landmarkName);
-        String name = temp.getText().toString();
-        if(name.length() == 0)
+        try
         {
-            errorVar += 1;
-        }
-        temp = findViewById(R.id.latitudeEntry);
-        String lat = temp.getText().toString();
-        rangeVal = Float.valueOf(lat);
-        if(rangeVal > 90 || rangeVal < -90)
-        {
-            errorVar += 2;
-        }
-        temp = findViewById(R.id.longitudeEntry);
-        String lon = temp.getText().toString();
-        rangeVal = Float.valueOf(lon);
-        if(rangeVal > 180 || rangeVal < -180)
-        {
-            errorVar += 4;
-        }
-        temp = findViewById(R.id.elevationEntry);
-        float elev = Float.parseFloat(temp.getText().toString());
-        if(elev > 8848 || elev < -420)
-        {
-            errorVar += 16;
-        }
-        //Date currDate = Calendar.getInstance().getTime();
-        temp = findViewById(R.id.notesEntry);
-        String note = temp.getText().toString();
+            float rangeVal;
+            EditText temp = findViewById(R.id.landmarkName);
+            String name = temp.getText().toString();
+            if (name.length() == 0)
+            {
+                throw new Exception("No name was entered");
+            }
+            else if(name.length() > 255)
+            {
+                throw new Exception("Name can only contain 255 characters at most.\nCurrent amount: " + name.length());
+            }
 
-        //CustomLocalLandmark addition = new CustomLocalLandmark(name, lat, lon, elev, "",currDate);
 
-        //CustomLocalLandmarkAccessorMethods_Impl add = new CustomLocalLandmarkAccessorMethods_Impl;
+            temp = findViewById(R.id.latitudeEntry);
+            String lat = temp.getText().toString();
+            if(lat.length() == 0)
+            {
+                throw new Exception("No latitude was entered");
+            }
+            else if(lat.length() > 20)
+            {
+                lat = lat.substring(0,20);
+            }
+            else if(lat.equals("-"))
+            {
+                throw new Exception("No value given to be negative");
+            }
+            rangeVal = Float.valueOf(lat);
+            if (rangeVal > 90 || rangeVal < -90)
+            {
+                throw new Exception("Latitude is not between -90 and 90");
+            }
+
+            temp = findViewById(R.id.longitudeEntry);
+            String lon = temp.getText().toString();
+            if(lon.length() == 0)
+            {
+                throw new Exception("No longitude was entered");
+            }
+            else if(lon.length() > 20)
+            {
+                lon = lon.substring(0,20);
+            }
+            else if(lon.equals("-"))
+            {
+                throw new Exception("No value given to be negative");
+            }
+            rangeVal = Float.valueOf(lon);
+            if (rangeVal > 180 || rangeVal < -180)
+            {
+                throw new Exception("Longitude is not between -180 and 180");
+            }
+
+            temp = findViewById(R.id.elevationEntry);
+            String eleVal = temp.getText().toString();
+            if(eleVal.length() == 0)
+            {
+                throw new Exception("No elevation was entered");
+            }
+            else if(lat.equals("-"))
+            {
+                throw new Exception("No value given to be negative");
+            }
+            float elev = Float.parseFloat(eleVal);
+            if (elev > 8848 || elev < -420)
+            {
+                throw new Exception("Elevation cannot reasonably be within these bounds");
+            }
+            //Date currDate = Calendar.getInstance().getTime();
+            temp = findViewById(R.id.notesEntry);
+            String note = temp.getText().toString();
+            if(note.length() > 1023)
+            {
+                throw new Exception("Notes can only contain 1023 characters at most.\nCurrent amount: " + note.length());
+            }
+
+            //CustomLocalLandmark addition = new CustomLocalLandmark(name, lat, lon, elev, "",currDate);
+
+            //CustomLocalLandmarkAccessorMethods_Impl add = new CustomLocalLandmarkAccessorMethods_Impl;
         /*CustomLocalLandmarkAccessorMethods_Impl add = new CustomLocalLandmarkAccessorMethods_Impl(AppDatabase.getM_DB_instance(this));
         add.insertCustomLandmarkStructure(addition);*/
 
-        //LandmarkedMain main =  LandmarkedMain.getInstance();
-        //main.insertCustomLandmarkStructure(addition);
+            //LandmarkedMain main =  LandmarkedMain.getInstance();
+            //main.insertCustomLandmarkStructure(addition);
 
-        AzureConnectionClass thePowerOfTheAzure = AzureConnectionClass.getAzureInstance();
-        //thePowerOfTheAzure.Connect();
-        thePowerOfTheAzure.InsertCustomLandmark(name, lat, lon, elev, "");
-        thePowerOfTheAzure.insertNote(note);
-        //thePowerOfTheAzure.Disconnect();
+            AzureConnectionClass thePowerOfTheAzure = AzureConnectionClass.getAzureInstance();
+            //thePowerOfTheAzure.Connect();
+            thePowerOfTheAzure.InsertCustomLandmark(name, lat, lon, elev, "");
+            thePowerOfTheAzure.insertNote(note);
+            //thePowerOfTheAzure.Disconnect();
 
-        finish();
+            finish();
+        }
+        catch(Exception e)
+        {
+            Context c = getApplicationContext();
+            Toast.makeText(c, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onCancelCustom(View v)
